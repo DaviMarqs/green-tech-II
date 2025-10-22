0aea116bb89965992c41d76c9909fd28ac34b3ca
Felipe Kenzo Ogaçawara Sunakozawa <felipe.sunakozawa@gmail.com>
2025-10-22 20:24:37 -0300

    Adicionando a tela de esqueci a senha

--- front/src/pages/Esqueci Senha/index.tsx
+++ front/src/pages/Esqueci Senha/index.tsx
@@ -1,11 +1,10 @@
-import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import api from "@/lib/api";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-export function Login() {
+export function Esqueci() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const handleLogin = async (event: React.FormEvent) => {
@@ -34,19 +33,8 @@ export function Login() {
         <div className="flex flex-col gap-6 w-auto lg:w-3/5 m-auto">
           <div className="flex-col w-fit">
             <h2 className="font-[Dm Sans] text-3xl text-center font-semibold justify-between">
-              Bem vindo(a) de volta!
+              Recuperar senha
             </h2>
-            <div className="flex justify-between">
-              <p className="flex w-full text-wrap:-nowrap pt-2">
-                Não possui uma conta?
-                <Link
-                  to="/register"
-                  className="ml-2 text-[#00C06B] text-wrap:-nowrap underline"
-                >
-                  Registre-se
-                </Link>
-              </p>
-            </div>
           </div>
           <div className="pt-3">
             <Label htmlFor="email" className="mb-2" id="email">
@@ -63,43 +51,54 @@ export function Login() {
             />
           </div>
           <div className="flex-col w-full">
-            <Label htmlFor="password" className="mb-2">
-              Senha
-            </Label>
-            <Input
-              className="flex center rounded-md border border-gray-300 w-full bg-white text-sm lg:text-base"
-              type="password"
-              placeholder="*****"
-              id="password"
-              name="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-            />
+            <div className="flex justify-between gap-5">
+                <div className="w-full">
+                    <Label htmlFor="password" className="mb-2">
+                    Nova senha
+                    </Label>
+                        <Input
+                        className="flex center rounded-md border border-gray-300 w-full bg-white text-sm lg:text-base"
+                        type="password"
+                        placeholder="*****"
+                        id="password"
+                        name="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        />
+                </div>
+                <div className="w-full">
+                    <Label htmlFor="confirmPassword" className="mb-2">
+                    Confirme a senha
+                    </Label>
+                        <Input
+                        className="flex center rounded-md border border-gray-300 w-full bg-white text-sm lg:text-base"
+                        type="password"
+                        placeholder="*****"
+                        id="confirmPassword"
+                        name="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        />
+                </div>
+            </div>
           </div>
           <div className="flex gap-2 justify-between">
             <div className="flex items-center gap-2">
-              <Checkbox
-                id="lembrarSenha"
-                className="w-4 h-4 hover:cursor-pointer"
-              />
-              <Label htmlFor="lembrarSenha" className="hover:cursor-pointer">
-                Manter conectado
-              </Label>
             </div>
-                <Link
-                  to="/forgot"
-                  className="ml-2 text-[#00C06B] text-wrap:-nowrap underline"
-                >
-                  Esqueci a senha
-                </Link>
           </div>
-          <div className="fazerLogin">
+          <div className="flex-col gap-2">
             <button
-              className="text-1xl bg-[#00C06B] text-white px-8 py-2 rounded-2xl w-full hover:cursor-pointer"
+              className="text-1xl bg-[#00C06B] text-white px-8 py-2 rounded-2xl w-full hover:cursor-pointer mb-5"
               onClick={handleLogin}
             >
-              Fazer login
+              Alterar senha
             </button>
+            <Link
+            to="/"
+              className="text-1xl bg-zinc-100 border-1 border-zinc-200 text-black px-8 py-2 rounded-2xl w-full hover:cursor-pointer block text-center"
+            >
+              Voltar
+            </Link>
           </div>
         </div>
       </div>
