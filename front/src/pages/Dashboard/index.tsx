ee7ac3d1e6148b30a7a14f95efa9efdf89276ae0
Felipe Sunakozawa <felipe.sunakozawa@gmail.com>
2025-11-01 11:00:18 -0300

    feat: adicionando cards de visualizacao de produto

--- front/src/pages/Dashboard/index.tsx
+++ front/src/pages/Dashboard/index.tsx
@@ -1,5 +1,16 @@
 import CreateProductCard from "@/components/CreateProductCard";
+import EditProductCard from "@/components/EditProductCard";
+import MyProductCard from "@/components/MyProductCard"
+import ProductCard from "@/components/ProductCard";
+
 
 export function Dashboard() {
-  return <CreateProductCard />;
+  return (
+    <section className="p-8">
+    <CreateProductCard />
+    <EditProductCard />
+    <MyProductCard />
+    <ProductCard />
+    </section>
+  )
 }
